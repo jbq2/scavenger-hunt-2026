455c5ebd149b202272b9d91034fc4980762e15d7
Joshua Quizon <jbq2.work@gmail.com>
2026-01-31 21:37:36 -0500

    fixing jackson puzzle path

--- src/Clue2Game.tsx
+++ src/Clue2Game.tsx
@@ -1,3 +1,4 @@
+import clueImage from "./assets/jackson-in-bin.jpeg";
 import { useState } from "react";
 import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
@@ -11,7 +12,7 @@ export default function Clue2Game() {
             <div style={{ width: "400px", height: "400px", marginBottom: "150px" }}>
                 <h2>Clue 2: Solve the puzzle!</h2>
                 <JigsawPuzzle
-                    imageSrc="/jackson-in-bin.jpeg"
+                    imageSrc={clueImage}
                     rows={3}
                     columns={3}
                     onSolved={() => setSolved(true)}
